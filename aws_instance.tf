d090930b328ad52f50e2e18db4fdb77de21057e2
henre1989 <transfer1989@gmail.com>
2021-03-10 17:35:48 +0500

    1

--- aws_instance.tf
+++ aws_instance.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "web" {
-  ami           = ami-08962a4068733a2b6
+  ami           = "ami-08962a4068733a2b6"
   instance_type = "t2.micro"
 
 }
